ce83a3fed7f2b3e5ed13e41984fef64f79456c88
kuralkardv-hue <kuralkardv@jdcoem.ac.in>
2026-09-03 10:53:34 +0530

    Add files via upload

---
+++ DSP.py
@@ -0,0 +1,21 @@
+# Student Data System
+
+# Taking input from the user
+name = input("Enter student name: ")
+roll_no = int(input("Enter roll number: "))
+age = int(input("Enter age: "))
+percentage = float(input("Enter percentage: "))
+
+# Displaying student information
+print("\n--- Student Data ---")
+print("Name:", name)
+print("Roll Number:", roll_no)
+print("Age:", age)
+print("Percentage:", percentage)
+
+# Displaying data types
+print("\n--- Data Types ---")
+print("Name type:", type(name))
+print("Roll Number type:", type(roll_no))
+print("Age type:", type(age))
+print("Percentage type:", type(percentage))
